21cd782db32eebb2fa2554c885c8cf38c8f76b04
Grozacodecode <shapovalovtima11111@gmail.com>
2024-10-27 23:01:42 +0300

    polished code

--- Read_Documents/grrr.c
+++ Read_Documents/grrr.c
@@ -69,19 +69,12 @@ bool isTextFile(const char *filename) {
 
 int main (int argc, char **argv) {
    
-   char a;
-   char b;
-   printf("Введите какой символ хотите заменить: ");
-   scanf("%d", &a);
-   printf("Ваш возраст: %d\n", a);
-
-
-
-
+  
     if (argc > 1) {
-        printf("Переданный файл: %s\n", argv[1]);
+      printf("Переданный файл: %s\n", argv[1]);
 
-      if (isTextFile(argv[1])) {
+      if (isTextFile(argv[1])) 
+      {
          printf("Файл %s является текстовым.\n", argv[1]);
         
          FILE *inputFile;
@@ -98,29 +91,35 @@ int main (int argc, char **argv) {
 
            
          inputFile = fopen(argv[1], "rb");
-         if (inputFile == NULL) {
+         if (inputFile == NULL) 
+         {
             printf("Не удалось открыть входной файл.\n");
             perror("Ошибка открытия файла");
             return 1;
          }
             
-         while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, inputFile)) > 0) {
+         while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, inputFile)) > 0) 
+         {
             snprintf(fileName, sizeof(fileName), "%s/part_%d.txt", exeDir, partNumber);
             outputFile = fopen(fileName, "wb");
-            if (outputFile == NULL) {
+            if (outputFile == NULL) 
+            {
             printf("Не удалось создать выходной файл %s.\n", fileName);
             fclose(inputFile);
             return 1;
+            }
          }
 
          fwrite(buffer, 1, bytesRead, outputFile);
          fclose(outputFile);
          partNumber++;
 
-         for (int i = 1; i < partNumber; i++) {
+         for (int i = 1; i < partNumber; i++) 
+         {
          snprintf(fileName, sizeof(fileName), "%s/part_%d.txt", exeDir, i);
          FILE *file = fopen(fileName, "rb+");
-         if (file == NULL) {
+         if (file == NULL) 
+         {
             printf("Не удалось открыть файл %s для изменения.\n", fileName);
             continue;
          }
@@ -132,8 +131,10 @@ int main (int argc, char **argv) {
 
          size_t bytesRead = fread(fileContent, 1, BUFFER_SIZE, file);
          
-         for (size_t j = 0; j < bytesRead; j++) {
-            if (fileContent[j] == ' ') {
+         for (size_t j = 0; j < bytesRead; j++) 
+         {
+            if (fileContent[j] == ' ') 
+            {
                 fileContent[j] = '_';
             }
          }
@@ -146,22 +147,25 @@ int main (int argc, char **argv) {
          fwrite(fileContent, 1, bytesRead, file);
          
          fclose(file);
-         
-         
-         
          }
-      }
-            
-            
-            
-            
+      
 
-      } else {
+      } 
+      
+      else 
+         {
             printf("Файл %s не является текстовым.\n", argv[1]);
-        }
-   } else {
+         }
+   }
+
+   else 
+
+
+   {
       printf("Файл не указан.\n");
       return 1;
    }
+
+
    return 0;
 }
